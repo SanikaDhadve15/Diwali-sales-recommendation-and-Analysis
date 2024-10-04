67bd2e51d1e7a91568c9d3d30736462bc1d9fd4d
Sanika Dhadve <140679118+SanikaDhadve15@users.noreply.github.com>
2024-10-04 23:32:29 +0530

    Add files via upload

--- data_upload_analysis.py
+++ data_upload_analysis.py
@@ -6,7 +6,7 @@ from PIL import Image
 def data_analysis():
     st.subheader("Data Upload & Analysis")
     # Display an image
-    image_path = "path/to/your/image.jpg"  # Replace with your image path
+    image_path = "C:\Users\Sanika\OneDrive\Desktop\SEM 7\Big data analaytics\BDAProject\di.jpg"  # Replace with your image path
     image = Image.open(image_path)
     st.image(image, caption='Diwali Sales Data', use_column_width=True)
 
