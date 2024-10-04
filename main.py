d66716a314c8550670c67e2a9e2151dbd763b10b
Sanika Dhadve <140679118+SanikaDhadve15@users.noreply.github.com>
2024-10-04 22:25:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from data_upload_analysis import data_analysis  # Ensure this matches your file name
-from visualizations import show_visualizations  # Ensure this matches your file name
-from recommendations import generate_recommendations  # Ensure this matches your file name
+from data_analysis import data_analysis
+from visualizations import show_visualizations
+from recommendations import show_recommendations
 
 def main():
     st.set_page_config(page_title="Diwali Sales Analysis")
@@ -10,11 +10,11 @@ def main():
     page = st.sidebar.radio("Select Page", ["Data Upload & Analysis", "Visualizations", "Recommendations"])
 
     if page == "Data Upload & Analysis":
-        data_analysis()  # Function to handle data upload and analysis
+        data_analysis()
     elif page == "Visualizations":
-        show_visualizations()  # Function to display various visualizations
+        show_visualizations()
     elif page == "Recommendations":
-        generate_recommendations()  # Function to generate product recommendations
+        show_recommendations()
 
 if __name__ == "__main__":
     main()
